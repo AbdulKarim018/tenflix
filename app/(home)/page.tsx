856bf086c0363bdaef427aab78a8e3482460c86d
AbdulKarim018 <lackhorse.ak@gmail.com>
2023-09-04 18:54:34 +0500

    some progress made

---
+++ app/(home)/page.tsx
@@ -0,0 +1,10 @@
+
+
+
+export default function Home() {
+  return (
+    <>
+      <h1 className="text-red-600 text-4xl">Tenflix</h1>
+    </>
+  )
+}
---
+++ app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,38 @@
+import CredentialsProvider from 'next-auth/providers/credentials';
+import NextAuth, { NextAuthOptions } from 'next-auth';
+
+const authOptions: NextAuthOptions = {
+    providers: [
+        CredentialsProvider({
+            id: "credentials",
+            name: "Credentials",
+            credentials: {
+                email: {
+                    label: "Email",
+                    placeholder: "john@email.com",
+                    type: "email",
+                },
+                password: {
+                    label: "Password",
+                    type: "password",
+                },
+            },
+            async authorize(credentials, req) {
+                const { email, password } = credentials as {
+                    email: string;
+                    password: string;
+                }
+                if (email && password) return { email, password };
+                return null
+            },
+        })
+    ],
+    pages: {},
+}
+
+const handler = NextAuth(authOptions);
+
+
+
+
+export { handler as GET, handler as POST }
