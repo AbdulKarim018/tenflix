9abc0e120fc765f9cb563239dd1acfab8c6186f7
AbdulKarim018 <125905086+AbdulKarim018@users.noreply.github.com>
2023-10-05 00:14:45 +0500

    Update MovieCard.tsx

--- components/MovieCard.tsx
+++ components/MovieCard.tsx
@@ -28,7 +28,7 @@ const MovieCard = ({ movie }: { movie: Movie }) => {
         <h3 className="text-white text-sm lg:text-lg font-semibold pointer-events-none p-1">{movie.title}</h3>
         <div className='w-full flex justify-end gap-2 p-1'>
           <Button variant={'ghost'} size={'icon'} ><BsFillBookmarkPlusFill className="w-5 h-5" /></Button>
-          <Button variant={'ghost'} size={'icon'} ><FaPlay className="w-5 h-5" /></Button>
+          <Button variant={'ghost'} size={'icon'} ><FaPlay className="w-5 h-5" />Play</Button>
         </div>
       </div>
     </div>
